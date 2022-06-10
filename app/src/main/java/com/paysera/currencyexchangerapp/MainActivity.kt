47cd882fe5d6e417693401106b6832a978607fb5
asad karami <asadkarami@asads-MacBook-Pro.local>
2022-06-10 18:12:18 +0430

    WIP

--- app/src/main/java/com/paysera/currencyexchangerapp/MainActivity.kt
+++ app/src/main/java/com/paysera/currencyexchangerapp/MainActivity.kt
@@ -3,8 +3,8 @@ package com.paysera.currencyexchangerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.paysera.currencyexchangerapp.presentation.screen.RatesScreen
 import com.paysera.currencyexchangerapp.core.ui.theme.CurrencyExchangerAppTheme
+import com.paysera.currencyexchangerapp.presentation.screen.RatesScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
